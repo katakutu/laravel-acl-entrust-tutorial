bd9d8ef1980425caa611b4bebc9be15a12602762
adyakbar <muh.adyakbar@gmail.com>
2016-12-21 09:50:13 +0700

    selesai membuat method crud UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,95 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+use App\Role;
+use Hash;
+use DB;
+
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-    	$data = \App\User::orderBy('id', 'DESC')->paginate(5);
+    	$data = User::orderBy('id', 'DESC')->paginate(5);
     	return view('users.index', compact('data'))
     		->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    public function create()
+    {
+    	$roles = Role::pluck('display_name', 'id');
+    	return view('users.create', compact('roles'));
+    }
+
+    public function store(Request $request)
+    {
+
+    	$this->validate($request, [
+    		'name' => 'required',
+    		'email' => 'required|email|unique:users, email',
+    		'password' => 'required|same:confirm-password',
+    		'roles' => 'required'
+    	]);
+
+    	$input = $request->all();
+    	$input['password'] = Hash::make($input['password']);
+
+    	$user = User::create($input);
+    	foreach($request->input('roles') as $key => $value) {
+    		$user->attachRole($value);
+    	}
+
+    	return redirect()->route('users.index')
+    					->with('success', 'User created successfully');
+    }
+
+    public function show($id)
+    {
+    	$user = User::find($id);
+    	return view('users.show', compact('user'));
+    }
+
+    public function edit($id)
+    {
+    	$user = User::find($id);
+    	$roles = Role::pluck('display_name', 'id');
+    	$userRole = $user->roles->lists('id', 'id')->toArray();
+
+    	return view('users.edit', compact('user', 'roles', 'userRole'));
+    }
+
+    public function update(Request $request, $id)
+    {
+    	$this->validate($request, [
+    		'name' => 'required',
+    		'email' => 'required|email|unique:users, email'.$id,
+    		'password' => 'same:confirm-password',
+    		'roles' => 'required'
+    	]);
+
+    	$input = $request->all();
+    	if(!empty($input['password'])) {
+    		$input['password'] = Hash::make($input['password']);
+    	} else {
+    		$input = array_except($input, array('password'));
+    	}
+
+    	$user = User::find($id);
+    	$user->update($input);
+    	DB::table('role_user')->where('user_id', $id)->delete();
+
+    	foreach($request->input('roles') as $key => $value) {
+    		$user->attachRole($value);
+    	}
+
+    	return redirect()->route('users.index')
+    					->with('success', 'User updated successfully');
+    }
+
+    public function delete($id)
+    {
+    	User::find($id)->delete();
+    	return redirect()->route('users.index')
+    					->with('success', 'User deleted successfully');
     }
 }
