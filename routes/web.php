c6ac63ae7fc8b64e440ff1bb869c10f7e8b2a835
adyakbar <muh.adyakbar@gmail.com>
2016-12-21 09:14:42 +0700

    selesai buat route sistem

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,39 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('roles/create', 'RoleController@create')
 		->name('roles.create')
 		->middleware('permission:role-create');
+	Route::post('roles/create', 'RoleController@store')
+		->name('roles.store')
+		->middleware('permission:role-create');
+	Route::get('roles/{id}', 'RoleController@show')
+		->name('roles.show');
+	Route::get('roles/{id}/edit', 'RoleController@edit')
+		->name('roles.edit')
+		->middleware('permission:role-edit');
+	Route::patch('roles/{id}', 'RoleController@update')
+		->name('roles.update')
+		->middleware('permission:role-edit');
+	Route::delete('roles/{id}', 'RoleController')
+		->name('roles.delete')
+		->middleware('permission:role-delete');
+
+	Route::get('items', 'ItemController@index')
+		->name('items.index')
+		->middleware('permission:item-list|item-create|item-edit|item-delete');
+	Route::get('items/create', 'ItemController@create')
+		->name('items.create')
+		->middleware('permission:item-create');
+	Route::post('items/store', 'ItemController@store')
+		->name('items.store')
+		->middleware('permission:item-create');
+	Route::get('items/{id}', 'ItemController@show')
+		->name('items.show');
+	Route::get('items/{id}/edit', 'ItemController@edit')
+		->name('items.edit')
+		->middleware('permission:item-edit');
+	Route::patch('items/{id}', 'RoleController@update')
+		->name('items.update');
+		->middleware('permission:item-edit');
+	Route::delete('items/{id}', 'RoleController@delete')
+		->name('items.delete');
+		->middleware('permission:item-delete');
 });
